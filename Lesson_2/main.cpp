3600bb3cd65ee3bf37a06437b74310b2a75b0d51
Roman Tkachuk <tkachukroman64@gmail.com>
2025-03-29 21:14:15 +0200

    Added small fixes for lection 2

--- Lesson_2/main.cpp
+++ Lesson_2/main.cpp
@@ -37,20 +37,20 @@ int main()
 
     // ========================================
 
-    int matrix[5][5];
-    for (int row = 0; row < 5; row++) {
-        for (int column = 0; column < 5; column++) {
-            matrix[row][column] = column + row;
-        }
-    }
+    // int matrix[5][5];
+    // for (int row = 0; row < 5; row++) {
+    //     for (int column = 0; column < 5; column++) {
+    //         matrix[row][column] = column + row;
+    //     }
+    // }
 
-    for (int row = 0; row < 5; row++) {
-        cout << "[" << row << "]: ";
-        for (int column = 0; column < 5; column++) {
-            cout << matrix[row][column] << " ";
-        }
-        cout << endl;
-    }
+    // for (int row = 0; row < 5; row++) {
+    //     cout << "[" << row << "]: ";
+    //     for (int column = 0; column < 5; column++) {
+    //         cout << matrix[row][column] << " ";
+    //     }
+    //     cout << endl;
+    // }
 
     // ========================================
 
